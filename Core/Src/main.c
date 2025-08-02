e07dc79c3e3770bd7b9cc6cfef44961914e7201e
0geder <0gedasam0k@gmail.com>
2025-08-03 01:12:33 +0200

    Initial commit: Practical 1A STM32 Developer Board Refresher

---
+++ Core/Src/main.c
@@ -0,0 +1,570 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include <stdint.h>
+#include "stm32f0xx.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim16;
+
+/* USER CODE BEGIN PV */
+// TODO: Define input variables
+uint8_t currentMode = 0; // 0=off, 1=back/forth, 2=inverse back/forth, 3=sparkle
+uint8_t currentLED = 0;  // Current LED position for modes 1 and 2
+uint8_t direction = 1;   // 1=forward, 0=backward for modes 1 and 2
+uint8_t delayMode = 0;   // 0=1s, 1=0.5s
+uint8_t sparkleState = 0; // 0=all off, 1=random on, 2=turning off
+uint8_t randomPattern = 0;
+uint32_t sparkleDelay = 0;
+uint8_t currentOffLED = 0;
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_TIM16_Init(void);
+/* USER CODE BEGIN PFP */
+void TIM16_IRQHandler(void);
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM16_Init();
+  /* USER CODE BEGIN 2 */
+
+  // TODO: Start timer TIM16
+  HAL_TIM_Base_Start_IT(&htim16); // Starting the timer in interrupt mode
+ 
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+
+    // TODO: Check pushbuttons to change timer delay
+	  if (LL_GPIO_IsInputPinSet(Button0_GPIO_Port, Button0_Pin)) {
+	      // Button0 (PA0) pressed - toggle delay mode
+	      delayMode = !delayMode;
+	      if (delayMode) {
+	          // Set to 0.5s delay
+	          htim16.Instance->ARR = 500-1;
+	      } else {
+	          // Set to 1s delay
+	          htim16.Instance->ARR = 1000-1;
+	      }
+	      // Simple debounce - wait for button release
+	      while (LL_GPIO_IsInputPinSet(Button0_GPIO_Port, Button0_Pin));
+	  }
+
+	  // Check other buttons for mode changes
+	  if (LL_GPIO_IsInputPinSet(Button1_GPIO_Port, Button1_Pin)) {
+	      currentMode = 1;
+	      currentLED = 0;
+	      direction = 1;
+	      while (LL_GPIO_IsInputPinSet(Button1_GPIO_Port, Button1_Pin));
+	  }
+
+	  if (LL_GPIO_IsInputPinSet(Button2_GPIO_Port, Button2_Pin)) {
+	      currentMode = 2;
+	      currentLED = 0;
+	      direction = 1;
+	      while (LL_GPIO_IsInputPinSet(Button2_GPIO_Port, Button2_Pin));
+	  }
+
+	  if (LL_GPIO_IsInputPinSet(Button3_GPIO_Port, Button3_Pin)) {
+	      currentMode = 3;
+	      sparkleState = 0;
+	      while (LL_GPIO_IsInputPinSet(Button3_GPIO_Port, Button3_Pin));
+	  }
+    
+
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_0);
+  while(LL_FLASH_GetLatency() != LL_FLASH_LATENCY_0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI_SetCalibTrimming(16);
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_HSI);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_HSI)
+  {
+
+  }
+  LL_SetSystemCoreClock(8000000);
+
+   /* Update the time base */
+  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief TIM16 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM16_Init(void)
+{
+
+  /* USER CODE BEGIN TIM16_Init 0 */
+
+  /* USER CODE END TIM16_Init 0 */
+
+  /* USER CODE BEGIN TIM16_Init 1 */
+
+  /* USER CODE END TIM16_Init 1 */
+  htim16.Instance = TIM16;
+  htim16.Init.Prescaler = 8000-1;
+  htim16.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim16.Init.Period = 1000-1;
+  htim16.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim16.Init.RepetitionCounter = 0;
+  htim16.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim16) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM16_Init 2 */
+  NVIC_EnableIRQ(TIM16_IRQn);
+  /* USER CODE END TIM16_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOF);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOA);
+  LL_AHB1_GRP1_EnableClock(LL_AHB1_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+
+  /**/
+  LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+  /**/
+  GPIO_InitStruct.Pin = Button0_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button0_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button1_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button1_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button2_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = Button3_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_UP;
+  LL_GPIO_Init(Button3_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED0_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED0_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED1_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED1_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED2_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED2_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED3_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED3_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED4_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED4_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED5_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED5_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED6_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED6_GPIO_Port, &GPIO_InitStruct);
+
+  /**/
+  GPIO_InitStruct.Pin = LED7_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(LED7_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+void TIM16_IRQHandler(void)
+{
+	// Acknowledge interrupt
+	HAL_TIM_IRQHandler(&htim16);
+
+	// TODO: Change LED pattern
+	void TIM16_IRQHandler(void)
+	{
+	    // Acknowledge interrupt
+	    HAL_TIM_IRQHandler(&htim16);
+
+	    // Change LED pattern based on current mode
+	    switch(currentMode) {
+	        case 0: // All off
+	            LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+	            LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+	            LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	            LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+	            LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+	            LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+	            LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+	            LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+	            break;
+
+	        case 1: // Mode 1: back/forth
+	            // Turn off all LEDs first
+	            LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+	            LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+	            LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	            LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+	            LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+	            LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+	            LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+	            LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+	            // Turn on current LED
+	            switch(currentLED) {
+	                case 0: LL_GPIO_SetOutputPin(LED0_GPIO_Port, LED0_Pin); break;
+	                case 1: LL_GPIO_SetOutputPin(LED1_GPIO_Port, LED1_Pin); break;
+	                case 2: LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin); break;
+	                case 3: LL_GPIO_SetOutputPin(LED3_GPIO_Port, LED3_Pin); break;
+	                case 4: LL_GPIO_SetOutputPin(LED4_GPIO_Port, LED4_Pin); break;
+	                case 5: LL_GPIO_SetOutputPin(LED5_GPIO_Port, LED5_Pin); break;
+	                case 6: LL_GPIO_SetOutputPin(LED6_GPIO_Port, LED6_Pin); break;
+	                case 7: LL_GPIO_SetOutputPin(LED7_GPIO_Port, LED7_Pin); break;
+	            }
+
+	            // Update position
+	            if (direction) {
+	                if (currentLED < 7) currentLED++;
+	                else {
+	                    direction = 0;
+	                    currentLED = 6; // Skip showing LED7 again
+	                }
+	            } else {
+	                if (currentLED > 0) currentLED--;
+	                else {
+	                    direction = 1;
+	                    currentLED = 1; // Skip showing LED0 again
+	                }
+	            }
+	            break;
+
+	        case 2: // Mode 2: inverse back/forth
+	            // Turn on all LEDs first
+	            LL_GPIO_SetOutputPin(LED0_GPIO_Port, LED0_Pin);
+	            LL_GPIO_SetOutputPin(LED1_GPIO_Port, LED1_Pin);
+	            LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	            LL_GPIO_SetOutputPin(LED3_GPIO_Port, LED3_Pin);
+	            LL_GPIO_SetOutputPin(LED4_GPIO_Port, LED4_Pin);
+	            LL_GPIO_SetOutputPin(LED5_GPIO_Port, LED5_Pin);
+	            LL_GPIO_SetOutputPin(LED6_GPIO_Port, LED6_Pin);
+	            LL_GPIO_SetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+	            // Turn off current LED
+	            switch(currentLED) {
+	                case 0: LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin); break;
+	                case 1: LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin); break;
+	                case 2: LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin); break;
+	                case 3: LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin); break;
+	                case 4: LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin); break;
+	                case 5: LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin); break;
+	                case 6: LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin); break;
+	                case 7: LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin); break;
+	            }
+
+	            // Update position
+	            if (direction) {
+	                if (currentLED < 7) currentLED++;
+	                else {
+	                    direction = 0;
+	                    currentLED = 6; // Skip showing LED7 again
+	                }
+	            } else {
+	                if (currentLED > 0) currentLED--;
+	                else {
+	                    direction = 1;
+	                    currentLED = 1; // Skip showing LED0 again
+	                }
+	            }
+	            break;
+
+	        case 3: // Mode 3: Sparkle
+	            if (sparkleState == 0) {
+	                // Generate random pattern
+	                randomPattern = (uint8_t)(HAL_GetTick() % 256);
+	                sparkleDelay = 100 + (HAL_GetTick() % 1400); // 100-1500ms
+
+	                // Set LEDs according to random pattern
+	                // Set LEDs based on randomPattern using if-else
+	                if (randomPattern & 0x01) LL_GPIO_SetOutputPin(LED0_GPIO_Port, LED0_Pin);
+	                else LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin);
+
+	                if (randomPattern & 0x02) LL_GPIO_SetOutputPin(LED1_GPIO_Port, LED1_Pin);
+	                else LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin);
+
+	                if (randomPattern & 0x04) LL_GPIO_SetOutputPin(LED2_GPIO_Port, LED2_Pin);
+	                else LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin);
+
+	                if (randomPattern & 0x08) LL_GPIO_SetOutputPin(LED3_GPIO_Port, LED3_Pin);
+	                else LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin);
+
+	                if (randomPattern & 0x10) LL_GPIO_SetOutputPin(LED4_GPIO_Port, LED4_Pin);
+	                else LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin);
+
+	                if (randomPattern & 0x20) LL_GPIO_SetOutputPin(LED5_GPIO_Port, LED5_Pin);
+	                else LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin);
+
+	                if (randomPattern & 0x40) LL_GPIO_SetOutputPin(LED6_GPIO_Port, LED6_Pin);
+	                else LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin);
+
+	                if (randomPattern & 0x80) LL_GPIO_SetOutputPin(LED7_GPIO_Port, LED7_Pin);
+	                else LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin);
+
+	                sparkleState = 1;
+	                // Adjust timer for random delay
+	                htim16.Instance->ARR = sparkleDelay-1;
+	            }
+	            else if (sparkleState == 1) {
+	                // Start turning off LEDs one by one
+	                sparkleState = 2;
+	                currentOffLED = 0;
+	                // Set timer for short delay between turning off LEDs
+	                htim16.Instance->ARR = (HAL_GetTick() % 100)-1;
+	            }
+	            else if (sparkleState == 2) {
+	                // Turn off next LED
+	                switch(currentOffLED) {
+	                    case 0: if (randomPattern & 0x01) LL_GPIO_ResetOutputPin(LED0_GPIO_Port, LED0_Pin); break;
+	                    case 1: if (randomPattern & 0x02) LL_GPIO_ResetOutputPin(LED1_GPIO_Port, LED1_Pin); break;
+	                    case 2: if (randomPattern & 0x04) LL_GPIO_ResetOutputPin(LED2_GPIO_Port, LED2_Pin); break;
+	                    case 3: if (randomPattern & 0x08) LL_GPIO_ResetOutputPin(LED3_GPIO_Port, LED3_Pin); break;
+	                    case 4: if (randomPattern & 0x10) LL_GPIO_ResetOutputPin(LED4_GPIO_Port, LED4_Pin); break;
+	                    case 5: if (randomPattern & 0x20) LL_GPIO_ResetOutputPin(LED5_GPIO_Port, LED5_Pin); break;
+	                    case 6: if (randomPattern & 0x40) LL_GPIO_ResetOutputPin(LED6_GPIO_Port, LED6_Pin); break;
+	                    case 7: if (randomPattern & 0x80) LL_GPIO_ResetOutputPin(LED7_GPIO_Port, LED7_Pin); break;
+	                }
+
+	                currentOffLED++;
+	                if (currentOffLED >= 8) {
+	                    sparkleState = 0;
+	                    // Reset timer to normal delay
+	                    if (delayMode) {
+	                        htim16.Instance->ARR = 500-1;
+	                    } else {
+	                        htim16.Instance->ARR = 1000-1;
+	                    }
+	                } else {
+	                    // Set timer for short delay between turning off LEDs
+	                    htim16.Instance->ARR = (HAL_GetTick() % 100)-1;
+	                }
+	            }
+	            break;
+	    }
+	}
+
+
+}
+
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
---
+++ README.md
@@ -0,0 +1,33 @@
+# Practical 1A - STM32F0xx Project
+
+This repository contains the STM32F0xx microcontroller project for Practical 1A.
+
+## Project Structure
+
+- `Core/` - Main application code
+  - `Inc/` - Header files
+  - `Src/` - Source files
+- `Drivers/` - STM32 HAL drivers
+- `Application/` - Application-specific code
+- `STM32CubeIDE/` - IDE configuration files
+
+## Hardware
+
+- Microcontroller: STM32F0xx series
+- Development board: STM32F051C6T6
+
+## Building the Project
+
+1. Open the project in STM32CubeIDE
+2. Build the project using the IDE's build tools
+3. Flash the generated binary to your STM32F0xx board
+
+## Dependencies
+
+- STM32CubeIDE
+- STM32F0xx HAL drivers
+- ARM GCC toolchain
+
+## License
+
+This project is part of EEE3096S coursework. 
